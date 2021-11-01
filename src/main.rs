09f7087e411ccd3948611645a60c03388ea30e1c
YutaGoto <you.goto.510@gmail.com>
2021-11-02 01:06:33 +0900

    add cors impl

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,35 @@ mod schema;
 mod services;
 
 use crate::handlers::{events_handler, users_handler};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
+
+pub struct CORS;
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    fn on_response(&self, _request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 fn main() {
     dotenv().ok();
     rocket::ignite()
+        .attach(CORS)
         .manage(connection::init_pool())
         .mount(
             "/events",
